487043dfec5a04cc4ed5b62fa4f51cbad0197bb2
Administrator <2369402392@qq.com>
2018-03-05 16:09:30 +0800

    提交项目到本地仓库

---
+++ src/main/java/pk1/Test.java
@@ -0,0 +1,12 @@
+package pk1;
+
+/**
+ * Created by Heyiyong on 2018/3/5/005.
+ */
+public class Test{
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaa");
+    }
+
+}
